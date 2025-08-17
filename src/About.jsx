8f4317737e83b42faa7dc89594279aae2ac5a403
ben_son <136886788+ben-24-0@users.noreply.github.com>
2025-08-17 20:02:39 +0530

    purple theme v1

--- src/About.jsx
+++ src/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div id="about" className="min-h-screen bg-black font-sans">
+    <div id="about" className="min-h-screen bg-black font-sans select-none">
       {/* Container for the purple section with wave SVGs. */}
       <div className="bg-purple-400">
         {/* Top SVG wave to create a curved top edge. */}
